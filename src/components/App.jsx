c3f88d093cf56c4f705c5161d6a099dd42ba9f0a
Kate Panchenko <k.panchenko12345@gmail.com>
2023-03-20 18:59:44 +0200

    Half done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
 import { theme } from './Layout';
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
-import { Layout } from "./Layout"
-// import { useEffect, useState } from 'react';
+
+import { Route, Routes } from "react-router-dom";
+import { About } from "../pages/About";
+import { Home } from "../pages/Home";
+import { ProductDetails } from "../pages/ProductDetails";
+import { Products } from "../pages/Products";
+import { Mission } from "./Mission";
+import { Team } from "./Team";
+import { Reviews } from "./Reviews";
+import { Container, Header, Logo, Link } from "./App.styled";
 
 export const App = () => {
   return (
-<ThemeProvider theme={theme}>
-        <Layout>
-          <GlobalStyle />
-          <div>React</div>
-        </Layout>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{" "}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </Container>
+    </ThemeProvider>
   );
 };
+
+// export const App = () => {
+//   return (
+//       <ThemeProvider theme={theme}>
+//         <Layout>
+//         <GlobalStyle />
+//         <nav>
+//         <StyledLink to="/" end>
+//           Home
+//         </StyledLink>
+//         <StyledLink to="/about">About</StyledLink>
+//         <StyledLink to="/products">Products</StyledLink>
+//       </nav>
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/about" element={<About />} />
+//           <Route path="/products" element={<Products />} />
+//           <Route path="/products/:productId" element={<ProductDetails />} />
+//           <Route path="*" element={<NotFound />} />
+//           </Routes>
+//         </Layout>
+//       </ThemeProvider>
+//   );
+// };
