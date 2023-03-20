a08c136ae18604aba88e61a11317bdf44668fae6
Kate Panchenko <k.panchenko12345@gmail.com>
2023-03-20 15:46:25 +0200

    template for react route test done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { theme } from './Layout';
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
+import { Layout } from "./Layout"
+// import { useEffect, useState } from 'react';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+<ThemeProvider theme={theme}>
+        <Layout>
+          <GlobalStyle />
+          <div>React</div>
+        </Layout>
+      </ThemeProvider>
   );
 };
